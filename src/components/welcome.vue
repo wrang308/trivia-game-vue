d30ebf33b71e09ba9ada9f6422174433373e93c8
wrang308 <markus.wrang_94@hotmail.com>
2021-08-09 16:59:59 +0200

    starting to add select menus to the start screen

---
+++ src/components/welcome.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        Hello component!
+
+        <select>
+        <option v-for="amount in amounts" :key="amount">{{amount}}</option>
+    
+        </select>
+        
+        <select>
+        <option v-for="diffulcty in difficulties" :key="diffulcty">{{diffulcty}}</option>
+    
+        </select>
+        
+    </div>
+</template>
+
+<script>
+
+
+
+export default {
+ name:'welcome',
+data(){
+    return{
+        amounts:[5,10,15,20,25,30,35,40,45,50],
+        difficulties:["easy", "medium", "hard"]
+    };
+}
+}
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,5 +2,22 @@
   <div>
     <div>hej!</div>
   <router-link :to="{ path: '/quiz' }">Quiz</router-link>
+  <welcome></welcome>
   </div>
 </template>
+
+<script>
+
+import Welcome from '../components/welcome.vue'
+
+export default{
+    name: 'home',
+    components:{
+        Welcome
+        
+    }
+}
+
+
+
+</script>
