92e68ae02888e3cff83bfe2db844ee64f19a4d04
Luis Gutierrez <luis.gutierrez@se.experis.com>
2021-08-11 17:11:37 +0200

    Created table with results info

--- src/components/QuizResults.vue
+++ src/components/QuizResults.vue
@@ -3,11 +3,18 @@
 Quiz results component
     <table>
       <thead>
-      <th></th>
-      <th></th>
-      <th></th>
+      <th>Question</th>
+      <th>Your Answer</th>
+      <th>Correct Answer</th>
       </thead>
+      <tr v-for="(item, index) in quizQuestions.results" :key="item">
+        <td v-html="item.question"></td>
+        <td v-bind:style="[userAnswers[index] === item.correct_answer ? {'color':'green'} : {'color':'red'}]">{{userAnswers[index]}}</td>
+        <td>{{item.correct_answer}}</td>
+      </tr>
     </table>
+    <div>Result : {{result}} points !</div>
+    <button @click="this.$router.go(-2)" v-html="'Got to Start'"/>
   </div>
 
 </template>
@@ -19,6 +26,19 @@ export default {
     userAnswers: [Object, Array],
     quizQuestions: [Object, Array],
     quiz: [Object, Array],
+  },
+  data(){
+    return{
+      result:0
+    }
+  },
+  created(){
+    for(let i = 0; i < this.quizQuestions.results.length; i++){
+      if(this.userAnswers[i] === this.quizQuestions.results[i].correct_answer){
+        this.result += 10
+      }
+
+    }
   }
 }
 </script>
